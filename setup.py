56975a2f71a24a14c46018bfd69b6e41a30c492d
terlyne <sheyynovd@gmail.com>
2024-10-01 14:16:00 +0300

    crazy

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'osm_cartography'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='OpenStreetMap visualization in RViz2',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'osm_cartography_node = osm_cartography.osm_cartography_node:main'
+        ],
+    },
+)
